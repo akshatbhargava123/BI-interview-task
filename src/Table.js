7033f823c11a51a0ad66deee3e4be672675e3bc4
akshat <akshatbhargavadeepa@gmail.com>
2019-08-10 17:47:45 +0530

    infinite scrolling now working fetching data from server

--- src/Table.js
+++ src/Table.js
@@ -3,25 +3,79 @@ import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 
+import { FakeServer, ServerSideDatasource } from './utils';
+
 class Table extends Component {
 	constructor(props) {
 		super(props);
+
 		this.state = {
 			columnDefs: [{
-				headerName: 'Make', field: 'make', sortable: true, filter: true
+				headerName: "ID",
+				width: 50,
+				valueGetter: "node.id",
+				suppressMenu: true,
+				sortable: false
+			},{
+				headerName: 'Name', field: 'athlete'
 			}, {
-				headerName: 'Model', field: 'model'
+				headerName: 'Age', field: 'age'
 			}, {
-				headerName: 'Price', field: 'price'
+				headerName: 'Country', field: 'country'
+			}, {
+				headerName: 'Date', field: 'date'
+			}, {
+				headerName: 'Sport', field: 'sport'
 			}],
-			rowData: [{
-				make: 'Toyota', model: 'Celica', price: 35000
-			}, {
-				make: 'Ford', model: 'Mondeo', price: 32000
-			}, {
-				make: 'Porsche', model: 'Boxter', price: 72000
-			}]
-		}
+      defaultColDef: {
+				resizable: true,
+				sortable: true,
+				filter: true,
+				cellRenderer: 'loadingCellRenderer'
+			},
+      components: {
+        loadingCellRenderer: function(params) {
+          if (params.value !== undefined) {
+            return params.value;
+          } else {
+            return '<img src="https://raw.githubusercontent.com/ag-grid/ag-grid/master/packages/ag-grid-docs/src/images/loading.gif">';
+          }
+        }
+      },
+      rowBuffer: 0,
+      rowSelection: "multiple",
+      rowModelType: "infinite",
+      paginationPageSize: 100,
+      cacheOverflowSize: 1,
+      maxConcurrentDatasourceRequests: 1,
+      infiniteInitialRowCount: 100,
+      maxBlocksInCache: 10
+		};
+
+		this.onGridReady = this.onGridReady.bind(this);
+	}
+
+	componentDidMount() {
+		
+	}
+
+	onGridReady(params) {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+
+    const updateData = data => {			
+			const fakeServer = FakeServer(data);
+			const dataSource = ServerSideDatasource(fakeServer);
+			params.api.setDatasource(dataSource);
+		};
+
+		// load all data to mock as paginated API on frontend
+		fetch('https://raw.githubusercontent.com/akshatbhargava123/BI-interview-task/master/src/assets/fakeData.json')
+			.then(res => res.json())
+			.then(data => {
+				console.log(data);
+				updateData(data);
+			});
 	}
 
 	render() {
@@ -30,13 +84,24 @@ class Table extends Component {
 				className="ag-theme-balham"
 				style={{
 					height: '500px',
-					width: '600px'
+					width: '800px'
 				}}
 			>
 				<AgGridReact
 					columnDefs={this.state.columnDefs}
-					rowData={this.state.rowData}>
-				</AgGridReact>
+					defaultColDef={this.state.defaultColDef}
+					components={this.state.components}
+					rowBuffer={this.state.rowBuffer}
+					rowSelection={this.state.rowSelection}
+					rowDeselection={true}
+					rowModelType={this.state.rowModelType}
+					paginationPageSize={this.state.paginationPageSize}
+					cacheOverflowSize={this.state.cacheOverflowSize}
+					maxConcurrentDatasourceRequests={this.state.maxConcurrentDatasourceRequests}
+					infiniteInitialRowCount={this.state.infiniteInitialRowCount}
+					maxBlocksInCache={this.state.maxBlocksInCache}
+					onGridReady={this.onGridReady}
+				/>
 			</div>
 		);
 	}
--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,9 @@
 export function ServerSideDatasource(server) {
   return {
+		rowCount: null,
     getRows(params) {
       setTimeout(function() {
-        var response = server.getResponse(params.request);
+        const response = server.getResponse(params);
         if (response.success) {
           params.successCallback(response.rows, response.lastRow);
         } else {
@@ -17,8 +18,8 @@ export function FakeServer(allData) {
   return {
     getResponse(request) {
       console.log("asking for rows: " + request.startRow + " to " + request.endRow);
-      var rowsThisPage = allData.slice(request.startRow, request.endRow);
-      var lastRow = allData.length <= request.endRow ? allData.length : -1;
+      const rowsThisPage = allData.slice(request.startRow, request.endRow);
+      const lastRow = allData.length <= request.endRow ? allData.length : -1;
       return {
         success: true,
         rows: rowsThisPage,
